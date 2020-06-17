594b6b23bb94ba23a1126e116e98363f83f9e72f
AnimMouse <git@animmouse.eu.org>
2020-06-17 15:39:06 +0800

    Create script for automation of new post

---
+++ new-posts.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+:Start
+echo Type the name of the new post
+echo Use dash for spacing
+set Name=
+set /p Name=Name:
+if NOT defined Name goto Start
+hugo new content\post\%Name%.md
+exit
